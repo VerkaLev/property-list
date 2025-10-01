2671bd5f00b577fd2b4c5f97f87acc978f21ba71
Veronika Klevko <90059559+VerkaLev@users.noreply.github.com>
2025-10-01 08:58:56 +0200

    maximum image height is removed

--- src/components/features/properties/PropertyInfo/Images.jsx
+++ src/components/features/properties/PropertyInfo/Images.jsx
@@ -3,10 +3,10 @@ import ImgWithFallback from '../../../ui/ImgWithFallback';
 export default function PropertyImages({ images }) {
   return (
     <div className='flex flex-col justify-end h-full gap-4 pb-4 md:pb-0'>
-      <div className='flex-[2] xl:max-h-[18rem] sm:min-h-[9rem] min-h-[12rem] rounded-xl bg-gray-200'>
+      <div className='flex-[2] w-full h-auto sm:min-h-[9rem] min-h-[12rem] rounded-xl bg-gray-200'>
         <ImgWithFallback src={images[0]} alt='property' />
       </div>
-      <div className='flex-[1] xl:max-h-[9rem] sm:min-h-[4rem] min-h-[7rem] flex gap-2'>
+      <div className='flex-[1] w-full h-auto sm:min-h-[4rem] min-h-[7rem] flex gap-2'>
         <div className='flex-1'>
           <ImgWithFallback src={images[1]} alt='property' />
         </div>
